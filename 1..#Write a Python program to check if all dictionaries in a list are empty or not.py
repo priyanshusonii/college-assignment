a82dd8925c11e2749117b8c17f0f4f679808b7e5
Priyanshu Soni <44646302+priyanshusonii@users.noreply.github.com>
2020-04-27 22:32:23 +0530

    Update 1..#Write a Python program to check if all dictionaries in a list are empty or not.py

--- 1..#Write a Python program to check if all dictionaries in a list are empty or not.py
+++ 1..#Write a Python program to check if all dictionaries in a list are empty or not.py
@@ -3,9 +3,9 @@ list_priyanshu2 = [{123,2345},{},{}]
 print(all(not d for d in list_priyanshu1))
 print(all(not d for d in list_priyanshu2))
 
-#
-output:
 
-True
-False
+#output:
+#
+#True
+#False
 #
